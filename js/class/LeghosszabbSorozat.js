46690eb2dd10407574621095ecedf5cb1f68bf1c
Adam <adam@mjoro.com>
2021-01-14 16:21:36 +0100

    LeghosszabbSorozat osztály létrehozás

---
+++ js/class/LeghosszabbSorozat.js
@@ -0,0 +1,9 @@
+class LeghosszabbSorozat {
+    constructor(numSeries, followingNumbers, id) {
+        this.numSeries = numSeries;
+        this.followingNumbers = followingNumbers;
+        this.id = id
+    }
+}
+
+export { LeghosszabbSorozat }
