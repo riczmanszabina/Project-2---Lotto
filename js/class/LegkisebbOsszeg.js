ae8d5aa127a759d516d76cc92182c47ded77d871
Adam <adam@mjoro.com>
2021-01-14 16:21:47 +0100

    LegkissebbOsszeg osztály létrehozás

---
+++ js/class/LegkisebbOsszeg.js
@@ -0,0 +1,9 @@
+class LegkisebbOsszeg {
+    constructor(sumResult, indexNum, lottoNumber) {
+        this.sumResult = sumResult;
+        this.indexNum = indexNum;
+        this.lottoNumber = lottoNumber;
+    }
+}
+
+export { LegkisebbOsszeg }
