9dfe61e0efa4e3810dc9d00a4c660b983d792373
Adam <adam@mjoro.com>
2021-01-19 15:42:58 +0100

    otosNyeremenyek.py bugfix

--- tools/otosNyeremenyek/otosNyeremenyek.py
+++ tools/otosNyeremenyek/otosNyeremenyek.py
@@ -9,11 +9,10 @@ with open ("otos.csv", 'r', encoding="utf-8") as data:
 for i in range(len(tempArry)):
     if (tempArry[i][4] != '0 Ft'):
         wonPrice = tempArry[i][4].replace("Ft", "").replace(" ", "")
-        wonPrice = wonPrice[0:len(wonPrice)-1]
         wonDate = tempArry[i][0] + " " + tempArry[i][1]
         resultArry.append(wonPrice + "\t" + wonDate)
 
 
-with open ("result.xlsx", "a", encoding="utf-8") as data:
+with open ("result.xls", "a", encoding="utf-8") as data:
     for i in range (len(resultArry)):
         data.write(resultArry[i] + "\n")
